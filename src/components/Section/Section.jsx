5432e031aa59a0da8157c33596764657a9699c76
Vitalij1989 <108751440+Vitalij1989@users.noreply.github.com>
2023-02-09 10:02:37 +0200

    1

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -7,12 +7,13 @@ import {
   Title,
 } from './Section.styled';
 
-export const Section = ({ title, children }) => {
+export const Section = ({ title, children, headerContent }) => {
   return (
     <PhonebookSection>
       <PhonebookContainer>
         <TitleContainer>
           <Title>{title}</Title>
+          {headerContent}
         </TitleContainer>
         {children}
       </PhonebookContainer>
